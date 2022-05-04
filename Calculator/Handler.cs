0026f77fef94caf0c284f2d654587ccc70bc6ed4
SzekacsB2 <bendeguz.szekacs@gmail.com>
2022-05-04 09:09:51 +0200

    Add project files.

---
+++ Calculator/Handler.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Evaluates an equation
+    /// </summary>
+    class Handler
+    {
+        public Handler()
+        {}
+
+        public double Evaluate(Token[] seq) //evaluates a tokenized array
+        {
+            seq = BracketHandler(seq);
+            seq = FunctionHandler(seq);
+            Token result = ArithmeticHandler(seq)[0];
+            return result.nValue;
+        }
+
+        #region Handlers
+
+        Token[] BracketHandler(Token[] seq) //deals with parentheses
+        {
+            int from = -1;
+            int count = 0;
+            for (int i = 0; i < seq.Length; i++)
+            {
+                if (seq[i].oValue == "(")
+                {
+                    if (from == -1) from = i;
+                    count++;
+                }
+                else if (seq[i].oValue == ")")
+                {
+                    count--;
+                }
+            }
+            if (count != 0) throw new Exception("ERROR Brackets- not paired");
+            if (from == -1) return seq;
+
+            int index = from + 1;
+            count = 1;
+            while (count != 0 && index < seq.Length)
+            {
+                if (seq[index].oValue == "(")
+                {
+                    count++;
+                }
+                else if (seq[index].oValue == ")")
+                {
+                    count--;
+                }
+                index++;
+            }
+            int to = index - 1;
+
+            Token[] slice = Slice(seq, from + 1, to - 1);
+            seq = Remove(seq, from, to, Evaluate(slice));
+            seq = BracketHandler(seq);
+            return seq;
+        }
+
+        Token[] ArithmeticHandler(Token[] seq) //deals with simple expressions
+        {
+            Exception e = new Exception("ERROR: Arithmetic- not computable");
+            //order 1
+            for (int i = 0; i < seq.Length; i++)
+            {
+                Token o = seq[i];
+                switch (o.oValue)
+                {
+                    case "^":
+                        if (seq[i - 1].oValue != null || seq[i + 1].oValue != null) throw e;
+                        double pow = Math.Pow(seq[i - 1].nValue, seq[i + 1].nValue);
+                        seq = Remove(seq, i - 1, i + 1, pow);
+                        i = -1;
+                        break;
+                    case "!":
+                        if (seq[i - 1].oValue != null) throw e;
+                        double n = seq[i - 1].nValue;
+                        if (n < 1) throw new Exception("ERROR: Arithmetic- factorial less than 1");
+                        if (n != Math.Floor(n)) throw new Exception("ERROR: Arithmetic- factorial decimal");
+                        double fac = 1;
+                        for (int j = 1; j < (int)n + 1; j++)
+                        {
+                            fac = j * fac;
+                        }
+                        seq = Remove(seq, i - 1, i, fac);
+                        i = -1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //order 2
+            for (int i = 0; i < seq.Length; i++)
+            {
+                Token o = seq[i];
+                switch (o.oValue)
+                {
+                    case "*":
+                        if (seq[i - 1].oValue != null || seq[i + 1].oValue != null) throw e;
+                        double mult = seq[i - 1].nValue * seq[i + 1].nValue;
+                        seq = Remove(seq, i - 1, i + 1, mult);
+                        i = -1;
+                        break;
+                    case "/":
+                        if (seq[i - 1].oValue != null || seq[i + 1].oValue != null) throw e;
+                        if (seq[i + 1].nValue == 0) throw new Exception("ERROR: Arithmetic- division by 0");
+                        double div = seq[i - 1].nValue / seq[i + 1].nValue;
+                        seq = Remove(seq, i - 1, i + 1, div);
+                        i = -1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            //order 3
+            for (int i = 0; i < seq.Length; i++)
+            {
+                Token o = seq[i];
+                switch (o.oValue)
+                {
+                    case "+":
+                        if (seq[i - 1].oValue != null || seq[i + 1].oValue != null) throw e;
+                        double add = seq[i - 1].nValue + seq[i + 1].nValue;
+                        seq = Remove(seq, i - 1, i + 1, add);
+                        i = -1;
+                        break;
+                    case "-":
+                        if (seq[i - 1].oValue != null || seq[i + 1].oValue != null) throw e;
+                        double sub = seq[i - 1].nValue - seq[i + 1].nValue;
+                        seq = Remove(seq, i - 1, i + 1, sub);
+                        i = -1;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            return seq;
+        }
+
+        Token[] FunctionHandler(Token[] seq) //deals with functions
+        {
+            for (int i = 0; i < seq.Length; i++)
+            {
+                Token f = seq[i];
+                if (f.isFunc)
+                {
+                    if (seq[i + 1].oValue != null) throw new Exception("ERROR: Function- not computable");
+                    switch (f.oValue) 
+                    {
+                        case "abs":
+                            double abs = Math.Abs(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, abs);
+                            break;
+                        case "cos":
+                            double cos = Math.Cos(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, cos);
+                            break;
+                        case "sin":
+                            double sin = Math.Sin(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, sin);
+                            break;
+                        case "tan":
+                            double tan = Math.Tan(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, tan);
+                            break;
+                        case "log":
+                            double log = Math.Log(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, log);
+                            break;
+                        case "sqr":
+                            double sqr = Math.Sqrt(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, sqr);
+                            break;
+                        case "clg":
+                            double clg = Math.Ceiling(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, clg);
+                            break;
+                        case "flr":
+                            double flr = Math.Floor(seq[i + 1].nValue);
+                            seq = Remove(seq, i, i + 1, flr);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+            return seq;
+        }
+
+        #endregion
+
+        #region Seq changers
+        public Token[] Slice(Token[] seq, int from, int to) //returns a given section of a tokenized array
+        {
+            List<Token> seqList = new List<Token>();
+
+            for (int i = from; i < to + 1; i++)
+            {
+                seqList.Add(seq[i]);
+            }
+
+            return seqList.ToArray();
+        }
+
+        public Token[] Remove(Token[] seq, int from, int to, double instead) //changes a section of a tokenized array into a value
+        {
+            List<Token> seqList = new List<Token>();
+
+            for (int i = 0; i < from; i++)
+            {
+                seqList.Add(seq[i]);
+            }
+            seqList.Add(new Token(instead));
+            for (int i = to + 1; i < seq.Length; i++)
+            {
+                seqList.Add(seq[i]);
+            }
+
+            return seqList.ToArray();
+        }
+
+        #endregion 
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Holds numbers, operations, brackets, and functions
+    /// </summary>
+    class Token
+    {
+        public double nValue { get; } //number value
+        public string oValue { get; } //operation value
+        public bool isFunc { get; }
+        public Token(double nValue)
+        {
+            this.nValue = nValue;
+            oValue = null;
+            isFunc = false;
+        }
+
+        public Token(string oValue, bool isFunc = false)
+        {
+            nValue = 0;
+            this.oValue = oValue;
+            this.isFunc = isFunc;
+        }
+    }
+
+    class Program
+    {
+        static Token[] Tokenize(string line) //converts a string into a tokenized array
+        {
+            List<Token> seq = new List<Token>();
+            List<bool> isNum = new List<bool>();
+            string num = "";
+            line = RemoveSpaces(line);
+            Exception e = new Exception("ERROR: Tokenizer- unknown character");
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                char c = line[i];
+                if (IsType(c, new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '.' }))
+                {
+                    if (c == '.') c = ',';
+                    num += c;
+                }
+                else if (IsType(c, new char[] { '+', '-', '*', '/', '^', '!', '(', ')' }))
+                {
+                    if (c == '-' && seq[seq.Count - 1].oValue == "(" && num == "")
+                    {
+                        seq.Add(new Token(-1));
+                        seq.Add(new Token("*"));
+                    }
+                    else
+                    {
+                        if (num != "")
+                        {
+                            seq.Add(new Token(Convert.ToDouble(num)));
+                        }
+                        num = "";
+                        seq.Add(new Token(c.ToString()));
+                    }
+                }
+                else
+                {
+                    if (c == 'p')
+                    {
+                        seq.Add(new Token(Math.PI));
+                    }
+                    else if (c == 'e')
+                    {
+                        seq.Add(new Token(Math.E));
+                    }
+                    else
+                    {
+                        if (i > line.Length - 3) throw e;
+                        string fun = c.ToString() + line[i + 1].ToString() + line[i + 2].ToString();
+
+                        switch (fun)
+                        {
+                            case "abs":
+                                seq.Add(new Token("abs", true));
+                                i += 2;
+                                break;
+                            case "cos":
+                                seq.Add(new Token("cos", true));
+                                i += 2;
+                                break;
+                            case "sin":
+                                seq.Add(new Token("sin", true));
+                                i += 2;
+                                break;
+                            case "tan":
+                                seq.Add(new Token("tan", true));
+                                break;
+                            case "log":
+                                seq.Add(new Token("log", true));
+                                break;
+                            case "sqr":
+                                seq.Add(new Token("sqr", true));
+                                break;
+                            case "clg":
+                                seq.Add(new Token("clg", true));
+                                i += 2;
+                                break;
+                            case "flr":
+                                seq.Add(new Token("flr", true));
+                                i += 2;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+
+            if (num != "")
+            {
+                seq.Add(new Token(Convert.ToDouble(num)));
+                isNum.Add(true);
+            }
+
+            return seq.ToArray();
+        }
+
+        static bool IsType(char symbol, char[] cArray) //determines if a character is of a given type
+        {
+            foreach (char c in cArray)
+            {
+                if (symbol == c) return true;
+            }
+            return false;
+        }
+
+        static string RemoveSpaces(string line)
+        {
+            string noSpace = "";
+            foreach (char c in line)
+            {
+                if (c != ' ') noSpace += c.ToString();
+            }
+            return noSpace;
+        }
+
+        #region Test
+        static void Test()
+        {
+            AssertEquals(2 + 3, "2 + 3");
+            AssertEquals(2 * 3.2, "2 * 3.2");
+            AssertEquals(4.5 * (-6) + 1, "4.5 * (-6)+ 1");
+            AssertEquals((-13 + 13) * 2, "(- 13 + 13) * 2");
+            AssertEquals((4 + (4.1 - 3) * 3) * (-2), "(4 + (4.1- 3)* 3) * (-2)");
+            AssertEquals(120, "5!");
+            AssertEquals(128, "4^3.5");
+            AssertEquals(97.2, "(-6*2)^2-(15.6*3)");
+        }
+
+        static void AssertEquals(double compare, string line)
+        {
+            Handler handler = new Handler();
+            try
+            {
+                double result = handler.Evaluate(Tokenize(line));
+                if (compare == result) return;
+                throw new Exception("wrong answer");
+            }
+            catch (Exception e)
+            {
+                string message = "ASSERT: " + e.Message + " in: " + line;
+                throw new Exception(message);
+            }
+        }
+        #endregion 
+
+        static void Main(string[] args)
+        {
+            Test();
+            Handler handler = new Handler();
+            bool isEnd = true;
+            string inp = Console.ReadLine();
+            while (isEnd)
+            {
+                Token[] seq = Tokenize(inp);
+                double result = handler.Evaluate(seq);
+                Console.WriteLine("result:" + result);
+                inp = Console.ReadLine();
+                if (inp == "") isEnd = false;
+            }
+        }
+    }
+}
